c9c3731df76ae39a697bb3a794b317aac5b317c5
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2011-01-05 12:27:25 +0200

    implemented buzz tests

---
+++ java-buzz-client/src/test/java/com/google/buzz/Config.java
@@ -0,0 +1,12 @@
+package com.google.buzz;
+
+public class Config
+{
+
+ static final String buzzToken="1/_HkgEIL7LZfx3iL0oVj6L8XniPzDS_g8qkH4VKUEqV0";
+ static final String buzzTokenSecret="K95E08Wu6KKnOQ33L3PiyjUJ";
+
+ static final String buzzConsumerKey="anonymous";
+ static final String buzzConsumerSecret="anonymous";
+
+}
